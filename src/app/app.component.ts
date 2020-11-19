6f6deffe2c049a0fa2c698aae9c6d64a94f234db
kenken64 <bunnyppl@hotmail.com@users.noreply.github.com>
2020-11-20 03:41:14 +0800

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray, FormGroupDirective } from '@angular/forms';
 import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './todo';
 
@@ -16,15 +16,19 @@ export class AppComponent implements OnInit{
   editTaskid: string;
   isEditMode: boolean = false;
 
+  taskFormControl = new FormControl('', [Validators.required]);
+  priorityFormControl = new FormControl('', [Validators.required]);
+  dueDateFormControl = new FormControl('', [Validators.required]);
+  
   constructor(private fb: FormBuilder){
     this.tomorrow.setDate(this.tomorrow.getDate() +1);
   }
 
   ngOnInit(){
     this.form = this.fb.group({
-      task: this.fb.control('', [Validators.required]),
-      priority: this.fb.control('', [Validators.required]),
-      dueDate: this.fb.control('', [Validators.required]),
+      task: this.taskFormControl,
+      priority: this.priorityFormControl,
+      dueDate: this.dueDateFormControl,
       todos: new FormArray([]),
     });
   }
@@ -35,18 +39,9 @@ export class AppComponent implements OnInit{
     }
   }
 
-  updateTodo(){
-
-  }
-
   addTodo(){
-    console.log("add todo");
-    console.log(this.form.value.task);
-    console.log(this.form.value.dueDate);
-    console.log(this.form.value.priority);
     const todosWithChkbox : FormArray = this.form.get('todos') as FormArray;
     let taskId = uuidv4();
-    console.log(taskId);
     let singleTodo = new Todo(
       this.form.value.task,
       this.form.value.priority,
@@ -55,26 +50,25 @@ export class AppComponent implements OnInit{
     );
     this.todoValues.push(singleTodo);
     const todoGroup = this.fb.group({
-      task: this.fb.control(false)
+      taskCheckbox: this.fb.control(false),
     });
     todosWithChkbox.push(todoGroup);
-    this.form.get('task').reset();
-    this.form.get('priority').reset();
-    this.form.get('dueDate').reset();
-    // get from localstorage then update the todo array??
+    this.taskFormControl.reset();
+    this.priorityFormControl.reset();
+    this.dueDateFormControl.reset();
     localStorage.setItem(taskId, JSON.stringify(singleTodo));
   }
   
   updateStatus(index){
-    this.todoValues[index].status=true;
+    if(this.todoValues[index].status){
+      this.todoValues[index].status=false;
+    }else{
+      this.todoValues[index].status=true;
+    }
   }
 
   onDelete(index, taskId){
-    console.log(index);
-    console.log(taskId);
-    
     const todosWithChkbox : FormArray = this.form.get('todos') as FormArray;
-    
     todosWithChkbox.removeAt(index);
     let deleteRecordFound = this.todoValues.findIndex((i)=> i.taskId === taskId);
     if(index > -1){
@@ -85,9 +79,37 @@ export class AppComponent implements OnInit{
 
   onEdit(index, taskId){
     console.log(index);
-    console.log(taskId);
     this.editTaskid = taskId;
     this.isEditMode = true;
+    let editTodoIdx = this.todoValues.findIndex((i)=> i.taskId === taskId);
+    this.form.patchValue(this.todoValues[editTodoIdx]); 
+  }
 
+  cancelEdit(formDirective? : FormGroupDirective){
+    this.isEditMode = false;
+    this.form.reset();
+    formDirective.resetForm();
+    Object.keys(this.form.controls).forEach(key => {
+      this.form.get(key).setErrors(null) ;
+    });
+    this.taskFormControl.setValidators([Validators.required]);
+    this.taskFormControl.updateValueAndValidity();
+    this.priorityFormControl.setValidators([Validators.required]);
+    this.priorityFormControl.updateValueAndValidity();
+    this.dueDateFormControl.setValidators([Validators.required]);
+    this.dueDateFormControl.updateValueAndValidity();
+  }
+  
+  updateEdit(){
+    let singleTodo = new Todo(
+      this.form.value.task,
+      this.form.value.priority,
+      this.form.value.dueDate,
+      this.editTaskid
+    );
+    let updateRecordFound = this.todoValues.findIndex((i)=> i.taskId === this.editTaskid);
+    this.todoValues[updateRecordFound] = singleTodo;
+    localStorage.setItem(this.editTaskid, JSON.stringify(singleTodo));
+    this.isEditMode = false; 
   }
 }
