566a78cdc5351366ad724bdb8ff9ce3ef84e2e1d
Kenneth Phang <bunnyppl@hotmail.com>
2020-11-19 13:04:25 +0800

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'todoApp';
+  form: FormGroup;
+  tomorrow = new Date();
+  
+  constructor(private fb: FormBuilder){
+    this.tomorrow.setDate(this.tomorrow.getDate() +1);
+  }
+
+  ngOnInit(){
+    this.form = this.fb.group({
+      task: this.fb.control('', [Validators.required]),
+      priority: this.fb.control('', [Validators.required]),
+      dueDate: this.fb.control('', [Validators.required])
+      
+    });
+  }
+
+  getPriorityErrorMessage(){
+    if (this.form.get('priority').hasError('required')) {
+      return 'You must enter a value';
+    }
+  }
+
+  addTodo(){
+
+  }
 }
